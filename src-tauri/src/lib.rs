b3c1f6ecf9e63d61b47c69bbca665ee37b8fa0a5
JSKitty <mail@jskitty.cat>
2025-01-06 21:55:09 +0000

    Initial Codebase - Barebones Edition This initial commit, with an attempt to keep it as small as possible, implements the necessary basic parts of a Nostr NIP-17 chat app, Chatstr! Let's see what the future brings, future commits will be much smaller and modular for easy review, educational and security purposes. Also, needless to say, the code is awful, this is an experiment.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,227 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use std::sync::Mutex;
+
+use nostr_sdk::prelude::*;
+use once_cell::sync::OnceCell;
+use lazy_static::lazy_static;
+
+static NOSTR_CLIENT: OnceCell<Client> = OnceCell::new();
+
+#[derive(serde::Serialize, Clone, Debug)]
+struct Message {
+    id: String,
+    content: String,
+    contact: String,
+    at: u64,
+    mine: bool,
+}
+
+#[derive(serde::Serialize, Clone, Debug)]
+struct Profile {
+    id: String,
+    name: String,
+    avatar: String,
+}
+
+struct ChatState {
+    messages: Vec<Message>,
+    profiles: Vec<Profile>,
+}
+
+impl ChatState {
+    fn new() -> Self {
+        Self { messages: Vec::new(), profiles: Vec::new() }
+    }
+
+    fn add_message(&mut self, message: Message) {
+        // Make sure we don't add the same message twice
+        if !self.messages.iter().any(|m| m.id == message.id) {
+            self.messages.push(message);
+        }
+    }
+
+    fn add_profile(&mut self, profile: Profile) {
+        // Make sure we don't add the same profile twice
+        if !self.profiles.iter().any(|m| m.id == profile.id) {
+            self.profiles.push(profile);
+        }
+    }
+}
+
+lazy_static! {
+    static ref STATE: Mutex<ChatState> = Mutex::new(ChatState::new());
+}
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+async fn fetch_messages() -> Result<Vec<Message>, ()> {
+    let client = NOSTR_CLIENT.get().expect("Nostr client not initialized");
+
+    // If we're not connected to any relays - connect and immediately retrieve our historical messages
+    if client.relays().await.len() == 0 {
+        connect().await;
+
+        // Grab our pubkey
+        let signer = client.signer().await.unwrap();
+        let pubkey = signer.get_public_key().await.unwrap();
+
+        // Fetch GiftWraps related to us
+        let filter = Filter::new().pubkey(pubkey).kind(Kind::GiftWrap);
+        let events = client
+            .fetch_events(vec![filter], std::time::Duration::from_secs(10))
+            .await.unwrap();
+
+        // Decrypt every GiftWrap and return their contents + senders
+        for maybe_dm in events.iter() {
+            if maybe_dm.kind == Kind::GiftWrap {
+                match client.unwrap_gift_wrap(&maybe_dm).await {
+                    Ok(UnwrappedGift { rumor, sender }) => {
+                        // Found a NIP-17 message!
+                        if rumor.kind == Kind::PrivateDirectMessage {
+                            let is_mine = sender == pubkey;
+                            // TODO: simplify and un-nest-ify this ugly section
+                            match rumor.tags.first() {
+                                Some(tag) => {
+                                    match tag.content() {
+                                        Some(content) => {
+                                            match PublicKey::from_hex(content) {
+                                                Ok(pub_key) => {
+                                                    match pub_key.to_bech32() {
+                                                        Ok(bech32) => {
+                                                            let msg = Message{ id: rumor.id.unwrap().to_bech32().unwrap(), content: rumor.content.to_string(), contact: if sender == pubkey { bech32 } else { sender.to_bech32().unwrap() }, at: rumor.created_at.as_u64(), mine: is_mine };
+                                                            let mut state = STATE.lock().unwrap();
+                                                            state.add_message(msg);
+                                                        },
+                                                        Err(_) => eprintln!("Failed to convert public key to bech32"),
+                                                    }
+                                                },
+                                                Err(_) => eprintln!("Failed to create public key from hex"),
+                                            }
+                                        },
+                                        None => eprintln!("No tag content"),
+                                    }
+                                },
+                                None => eprintln!("No tags found"),
+                            }                            
+                        }
+                    }
+                    Err(_e) => (),
+                }
+            }
+        }
+    }
+
+    let state = STATE.lock().unwrap();
+    let msgs = state.messages.clone();
+
+    Ok(msgs)
+}
+
+#[tauri::command]
+async fn message(receiver: String, content: String) -> Result<bool, ()> {
+    let client = NOSTR_CLIENT.get().expect("Nostr client not initialized");
+
+    // Convert the Bech32 String in to a PublicKey
+    let receiver_pubkey = PublicKey::from_bech32(receiver.as_str()).unwrap();
+
+    // Attempt to send the message
+    match client.send_private_msg(receiver_pubkey, content.clone(), []).await {
+        Ok(response) => {
+            let msg = Message{ id: response.id().to_bech32().unwrap(), content: content, contact: receiver, at: std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap().as_secs(), mine: true };
+            let mut state = STATE.lock().unwrap();
+            state.add_message(msg);
+            return Ok(true);
+        },
+        Err(e) => {
+            eprintln!("Error: {:?}", e);
+            return Ok(false);
+        },
+    }
+}
+
+#[tauri::command]
+async fn load_profile(npub: String) -> Result<Profile, ()> {
+    let client = NOSTR_CLIENT.get().expect("Nostr client not initialized");
+
+    // Convert the Bech32 String in to a PublicKey
+    let profile_pubkey = PublicKey::from_bech32(npub.as_str()).unwrap();
+
+    // Attempt to fetch their Metadata profile
+    match client.fetch_metadata(profile_pubkey, std::time::Duration::from_secs(10)).await {
+        Ok(response) => {
+            let profile = Profile{ id: npub, name: response.name.unwrap_or_default(), avatar: response.picture.unwrap_or_default() };
+            let mut state = STATE.lock().unwrap();
+            state.add_profile(profile.clone());
+            return Ok(profile);
+        },
+        Err(e) => {
+            eprintln!("Error: {:?}", e);
+            return Err(());
+        },
+    }
+}
+
+#[tauri::command]
+async fn notifs() {
+    let client = NOSTR_CLIENT.get().expect("Nostr client not initialized");
+
+    // Grab our pubkey
+    let signer = client.signer().await.unwrap();
+    let pubkey = signer.get_public_key().await.unwrap();
+
+    // Listen for GiftWraps related to us
+    let filter = Filter::new().pubkey(pubkey).kind(Kind::GiftWrap).limit(0);
+
+    // Subscribe to the filter and begin handling incoming events
+    client.subscribe(vec![filter], None).await.unwrap();
+    client
+        .handle_notifications(|notification| async {
+            if let RelayPoolNotification::Event { event, .. } = notification {
+                if event.kind == Kind::GiftWrap {
+                    match client.unwrap_gift_wrap(&event).await {
+                        Ok(UnwrappedGift { rumor, sender }) => {
+                            if rumor.kind == Kind::PrivateDirectMessage {
+                                let msg = Message{ id: rumor.id.unwrap().to_bech32().unwrap(), content: rumor.content.to_string(), contact: sender.to_bech32().unwrap().to_string(), at: rumor.created_at.as_u64(), mine: pubkey == rumor.pubkey };
+                                let mut state = STATE.lock().unwrap();
+                                state.add_message(msg);
+                            }
+                        }
+                        Err(_e) => (),
+                    }
+                }
+            }
+            Ok(false)
+        })
+        .await.unwrap();
+}
+
+#[tauri::command]
+async fn login(npub: String) -> Result<bool, ()> {
+    // TODO: add validation, error handling, mnemonic phrase support, etc
+    let keys = Keys::parse(&npub).unwrap();
+    let client = Client::builder()
+        .signer(keys.clone())
+        .opts(Options::new().gossip(false))
+        .build();
+    NOSTR_CLIENT.set(client).unwrap();
+    Ok(true)
+}
+
+async fn connect() {
+    let client = NOSTR_CLIENT.get().expect("Nostr client not initialized");
+
+    // Add a couple common relays, especially with explicit NIP-17 support (thanks 0xchat!)
+    client.add_relay("wss://relay.0xchat.com").await.unwrap();
+    client.add_relay("wss://auth.nostr1.com").await.unwrap();
+    client.add_relay("wss://relay.damus.io").await.unwrap();
+
+    // Connect!
+    client.connect().await;
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![fetch_messages, message, login, notifs, load_profile])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.js
+++ src/main.js
@@ -1,18 +1,238 @@
 const { invoke } = window.__TAURI__.core;
 
-let greetInputEl;
-let greetMsgEl;
+const domLogin = document.getElementById('login-form');
+const domLoginInput = document.getElementById('login-input');
+const domLoginBtn = document.getElementById('login-btn');
 
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+const domChats = document.getElementById('chats');
+
+const domChat = document.getElementById('chat');
+const domChatBackBtn = document.getElementById('chat-back-btn');
+const domChatContact = document.getElementById('chat-contact');
+const domChatMessages = document.getElementById('chat-messages');
+const domChatMessageBox = document.getElementById('chat-box');
+const domChatMessageInput = document.getElementById('chat-input');
+
+/**
+ * @typedef {Object} Message
+ * @property {string} content - The content of the message.
+ * @property {string} contact - The identifier of the contact.
+ * @property {number} at - The timestamp of the message.
+ * @property {boolean} mine - Whether the message was sent by us, or them.
+ */
+
+/**
+ * @typedef {Object} Chat
+ * @property {string} contact - The id of the contact.
+ * @property {Message[]} contents - Array of messages associated with the contact.
+ */
+
+/**
+ * Organizes an array of Message objects by contact into an array of Chat objects.
+ * Each contact in the Chat array has an array of associated message contents.
+ *
+ * @param {Message[]} data - The data to be sorted.
+ * @returns {Chat} - The organized data.
+ */
+function sortTocontact(data) {
+    // Sort the messages in ascending order of timestamps
+    data.sort((a, b) => a.at - b.at);
+
+    // Create an empty object to collect contact data for sorting
+    let contactData = {};
+
+    // Iterate through every item in the data array
+    data.forEach(item => {
+
+        // If the contact doesn't exist in contactData yet, create a new array for them
+        if (!(item.contact in contactData)) {
+            contactData[item.contact] = [];
+        }
+
+        // Add the message to the chat data
+        contactData[item.contact].push(item);
+    });
+
+    // Create an array of Chats from contactData
+    return Object.entries(contactData).map(([contact, contents]) => ({ contact, contents }));
+}
+
+/**
+ * A cache of all chats with linear chronological history
+ * @type {Chat[]}
+ */
+let arrChats = [];
+
+/**
+ * A cache of all profile metadata for folks we've chat with
+ */
+let arrProfiles = [];
+
+/**
+ * The current open chat (by npub)
+ */
+let strOpenChat = "";
+
+/**
+ * Fetch all messages from the client
+ * 
+ * **Note:** Setting 'init' simply starts an automatic re-call every half-second
+ * to emulate a "live" feed, this could probably be improved later.
+ * 
+ * **Note:** Only the first call actually calls to the Nostr network, all 
+ * consecutive calls utilise cache, which is updated by the event (notify) system.
+ * 
+ * @param {boolean} init - Whether this is an Init call or not
+ */
+async function fetchMessages(init = false) {
+    if (init) {
+        domChats.textContent = `Loading DMs...`;
+    }
+    const arrMessages = await invoke("fetch_messages");
+
+    // Sort our linear message history in to Chats
+    arrChats = sortTocontact(arrMessages);
+
+    // If a chat is open, update it's messages
+    if (strOpenChat) {
+        updateChat(strOpenChat);
+    }
+
+    // Render the chats
+    domChats.innerHTML = ``;
+    for (const chat of arrChats) {
+        // Let's try to load the profile of each chat, too
+        let cProfile = arrProfiles.find(a => a.id === chat.contact);
+        if (!cProfile) {
+            try {
+                if (init) {
+                    domChats.textContent = `Loading Contact Profile...`;
+                }
+                cProfile = await invoke("load_profile", { npub: chat.contact });
+                arrProfiles.push(cProfile);
+            } catch (e) {
+                arrProfiles.push({ id: chat.contact, name: '', avatar: '' });
+            }
+        }
+        // The Contact container
+        const divContact = document.createElement('div');
+        divContact.classList.add('chatlist-contact');
+        divContact.onclick = () => { openChat(chat.contact) };
+
+        // The avatar, if one exists
+        if (cProfile?.avatar) {
+            const imgAvatar = document.createElement('img');
+            imgAvatar.src = cProfile.avatar;
+            divContact.appendChild(imgAvatar);
+        }
+
+        // The name (or, if missing metadata, their npub instead)
+        const h3ContactName = document.createElement('h3');
+        h3ContactName.textContent = cProfile?.name || chat.contact;
+
+        // Slap it all together
+        divContact.appendChild(h3ContactName);
+        domChats.appendChild(divContact);
+    }
+
+    // Start a post-init refresh loop, which will frequently poll cached chats from the client
+    if (init) setInterval(fetchMessages, 500);
+}
+
+/**
+ * Send a NIP-17 message to a Nostr user
+ * @param {string} pubkey - The user's pubkey
+ * @param {string} content - The content of the message
+ */
+async function message(pubkey, content) {
+    await invoke("message", { receiver: pubkey, content: content });
+}
+
+/**
+ * Login to the Nostr network
+ */
+async function login() {
+    const fLoggedIn = await invoke("login", { npub: domLoginInput.value });
+    if (fLoggedIn) {
+        // Hide the login UI
+        domLoginInput.value = "";
+        domLogin.style.display = 'none';
+
+        // Connect and fetch historical messages
+        await fetchMessages(true);
+
+        // Setup a subscription for new websocket messages
+        invoke("notifs");
+    }
+}
+
+/**
+ * Open a chat with a particular contact
+ * @param {string} contact 
+ */
+function openChat(contact) {
+    // Display the Chat UI
+    domChats.style.display = 'none';
+    domChat.style.display = '';
+
+    // Render the current contact's messages
+    strOpenChat = contact;
+    updateChat(contact);
+}
+
+/**
+ * A simple state tracker for the last message ID, if it changes, we auto-scroll
+ */
+let strLastMsgID = "";
+
+/**
+ * Updates the current chat (to display incoming and outgoing messages)
+ * @param {string} contact 
+ */
+function updateChat(contact) {
+    const cContact = arrChats.find(a => a.contact === contact);
+    const cProfile = arrProfiles.find(a => a.id === contact);
+    if (cContact) {
+        // Prefer displaying their name, otherwise, npub
+        domChatContact.textContent = cProfile?.name || contact;
+
+        // Render their messages
+        const arrMessages = cContact.contents;
+        domChatMessages.innerHTML = ``;
+        for (const msg of arrMessages) {
+            domChatMessages.innerHTML += `<p class="msg-${msg.mine ? 'me' : 'them'}">${msg.content}</p>`;
+        }
+
+        // Auto-scroll on new messages (not a great implementation)
+        if (arrMessages.length) {
+            const cLastMsg = arrMessages[arrMessages.length - 1];
+            if (strLastMsgID !== cLastMsg.id) {
+                domChatMessages.scrollTo(0, domChatMessages.scrollHeight);
+                strLastMsgID = cLastMsg.id;
+            }
+        }
+    }
+}
+
+/**
+ * Closes the current chat, taking the user back to the chat list
+ */
+function closeChat() {
+    domChats.style.display = '';
+    domChat.style.display = 'none';
+    strOpenChat = "";
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
-  });
+    // Hook up our static buttons
+    domLoginBtn.onclick = login;
+    domChatBackBtn.onclick = closeChat;
+
+    // Hook up an 'Enter' listener on the Message Box for sending them
+    domChatMessageInput.onkeydown = async (evt) => {
+        if (evt.code === 'Enter' && domChatMessageInput.value.trim().length) {
+            await message(domChatContact.textContent, domChatMessageInput.value);
+            domChatMessageInput.value = '';
+        }
+    }
 });
